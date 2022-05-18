35c4acd1d2a1da057bc30f6632275e38fe5d0878
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-05-18 18:43:51 -0300

    feat: add nova tarefa a lista de tarefas

--- src/componets/AddTodo/index.jsx
+++ src/componets/AddTodo/index.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddTodo() {
+  const [task, setTask] = useState('');
+  const [listask, setLisTask] = useState([]);
+
+  const handleInputChange = ({ target }) => {
+    const { value } = target;
+    setTask(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLisTask([...listask, task]);
+    setTask('');
+  };
+
   return (
     <section>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="todo-input"
+          value={task}
+          onChange={handleInputChange}
         />
-        <button type="submit">
+        <button
+          type="submit"
+        >
           Add Todo
         </button>
       </form>
