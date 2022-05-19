17cb460d95586ebe8e0d9628e7db42e7142dc6d9
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-05-19 13:00:16 -0300

    style: component NewTodo

--- src/componets/NewTodo/index.jsx
+++ src/componets/NewTodo/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import './NewTodo.css';
 
 function NewTodo() {
   return (
     <div className="new-todo">
-      <input type="checkbox" name="" id="" />
-      <p>NOVA TAREFA</p>
-      <button type="button">X</button>
-      <button type="button">Edit</button>
+      <div className="check-task">
+        <input type="checkbox" name="" id="" />
+        <p>NOVA TAREFA</p>
+      </div>
+      <div className="del-edit">
+        <button type="button">
+          <i className="bi bi-x-circle" />
+        </button>
+        <button type="button">
+          <i className="bi bi-pencil-square" />
+        </button>
+      </div>
     </div>
   );
 }
