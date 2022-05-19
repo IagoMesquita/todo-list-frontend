97b11ba2831d813da1f541830fc500de6f7b69ba
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-05-19 16:41:06 -0300

    feat: componente addTodo-

--- src/componets/AddTodo/index.jsx
+++ src/componets/AddTodo/index.jsx
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
-import './addTodo.css';
-import NewTodo from '../NewTodo';
+// import React, { useState } from 'react';
+// import './addTodo.css';
+// import NewTodo from '../NewTodo';
 
-function AddTodo() {
-  const [task, setTask] = useState('');
-  const [listTask, setLisTask] = useState([]);
+// function AddTodo() {
+//   const [task, setTask] = useState('');
+//   const [listTask, setLisTask] = useState([]);
 
-  const handleInputChange = ({ target }) => {
-    const { value } = target;
-    setTask(value);
-  };
+//   const handleInputChange = ({ target }) => {
+//     const { value } = target;
+//     setTask(value);
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLisTask([...listTask, task]);
-    setTask('');
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setLisTask([...listTask, task]);
+//     setTask('');
+//   };
 
-  return (
-    <main className="container-content">
-      <form
-        className="container-input"
-        onSubmit={handleSubmit}
-      >
-        <input
-          className="shearch"
-          placeholder="Sua tarefa"
-          type="text"
-          name="todo-input"
-          value={task}
-          onChange={handleInputChange}
-        />
-        <button
-          className="btn"
-          type="submit"
-        >
-          Add Todo
-        </button>
-      </form>
-      <section>
-        {
-          listTask.map((item) => (
-            <NewTodo todo={item} />
-          ))
-        }
-      </section>
+//   return (
+//     <main className="container-content">
+//       <form
+//         className="container-input"
+//         onSubmit={handleSubmit}
+//       >
+//         <input
+//           className="shearch"
+//           placeholder="Sua tarefa"
+//           type="text"
+//           name="todo-input"
+//           value={task}
+//           onChange={handleInputChange}
+//         />
+//         <button
+//           className="btn"
+//           type="submit"
+//         >
+//           Add Todo
+//         </button>
+//       </form>
+//       <section>
+//         {
+//           listTask.map((item) => (
+//             <NewTodo todo={item} />
+//           ))
+//         }
+//       </section>
 
-    </main>
-  );
-}
+//     </main>
+//   );
+// }
 
-export default AddTodo;
+// export default AddTodo;
