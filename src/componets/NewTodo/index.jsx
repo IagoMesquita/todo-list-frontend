56f3645c10fb4edc229da5feb609d8de0b772adc
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-05-19 16:42:51 -0300

    feat: component newTodo

--- src/componets/NewTodo/index.jsx
+++ src/componets/NewTodo/index.jsx
@@ -1,28 +1,28 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import './NewTodo.css';
+// import PropTypes from 'prop-types';
+// import React from 'react';
+// import './NewTodo.css';
 
-function NewTodo({ todo }) {
-  return (
-    <div className="new-todo">
-      <div className="check-task">
-        <input type="checkbox" name="" id="" />
-        <p>{todo}</p>
-      </div>
-      <div className="del-edit">
-        <button type="button">
-          <i className="bi bi-x-circle" />
-        </button>
-        <button type="button">
-          <i className="bi bi-pencil-square" />
-        </button>
-      </div>
-    </div>
-  );
-}
+// function NewTodo({ todo }) {
+//   return (
+//     <div className="new-todo">
+//       <div className="check-task">
+//         <input type="checkbox" name="" id="" />
+//         <p>{todo}</p>
+//       </div>
+//       <div className="del-edit">
+//         <button type="button">
+//           <i className="bi bi-x-circle" />
+//         </button>
+//         <button type="button">
+//           <i className="bi bi-pencil-square" />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 
-NewTodo.propTypes = {
-  todo: PropTypes.string.isRequired,
-};
+// NewTodo.propTypes = {
+//   todo: PropTypes.string.isRequired,
+// };
 
-export default NewTodo;
+// export default NewTodo;
