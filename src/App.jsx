2b9055f7ddc03a6c675f5017225b85ffac096096
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-05-19 17:25:37 -0300

    feat: componente cabecalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './componets/Header';
 import AddTodo from './componets/AddTodo';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AddTodo />
     </div>
   );
